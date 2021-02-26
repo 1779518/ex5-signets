18431a1dc4f8fe690bf6536efdc31227f830222e
1779518 <suggestedmailname@gmail.com>
2021-02-26 01:28:44 -0500

    ajout du projet

---
+++ src/composants/Dossier.jsx
@@ -0,0 +1,22 @@
+import './Dossier.scss';
+import SortIcon from '@material-ui/icons/Sort';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+export default function Dossier({id, titre, couleur, dateModif}) {
+  return (
+    <article className="Dossier"
+      style={{
+        backgroundColor: `${couleur}`,
+      }}>
+      <div className="couverture">
+        <span className="deplacer"><SortIcon></SortIcon></span>
+        <img src={`images-dossiers/${id}.png`} alt={titre}/>
+      </div>
+      <div className="info">
+        <h2>{titre}</h2>
+        <p>Modifié: {dateModif}</p>
+      </div>
+      <button><MoreVertIcon></MoreVertIcon></button>
+    </article>
+  );
+}
